66f3f40577b7bc1f5c24022a7f47c2e57bd8b75a
billythemusical <bennett.billy@gmail.com>
2020-06-18 18:20:12 -0400

    Working with basic queries now.

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/*
+
+June 2020
+
+Built in cooperation with Tech & Society and The Coding Lab at NYU's ITP program.
+
+The Rita.js portion is adapted from Allison Parrish's tutorial:
+https://creative-coding.decontextualize.com/intro-to-ritajs/
+
+*/
+
+
+
 console.log(
   `-----------------
   Welcome to Conversation Starter!
@@ -7,23 +20,32 @@ console.log(
 /*
 
 Firebase Code to init our database, list our users, store any data.
+Could also port the statistics page to Google Sheets for ease.
 
 */
 
 // Import in our admin messages
 const { adminMessages } = require('./admin-messages');
 const { statistics } = require('./statistics');
+const RiTa = require('RiTa')
+require('dotenv').config();
+let twilio = require('twilio');
+
+// let msg = "Is there a problem with wealth inequality in the United States?"
+// let msg = "Is there a problem with health in the United States?"
+// let keywords = getKeywords(msg);
+// Find the matches
+// let matches = matchSearch(keywords);
+// console.log(random(matches))
+// process.exit(1)
+
 
 // An array to hold the phone numbers
 let numbers = [];
-const port = 3001
-
-require('dotenv').config();
+const port = 8081
 
 const adminNum = process.env.ADMIN_NUMBER;
 const appNum = process.env.TWILIO_NUMBER;
-
-let twilio = require('twilio');
 
 let client = new twilio(
   process.env.ACCOUNT_SID,
@@ -62,25 +84,35 @@ app.post('/sms', function (req, res) {
   // Get the SMS message text
   let msg = req.body.Body;
   console.log(`SMS Received from: ${fromNum}\nMessage: ${msg}`);
+  var msgTrim = msg.trim().toLowerCase();
+  var firstWord = msg.split(' ')[0].toLowerCase();
 
   // Make a new variable to respond with
   let resp = new MessagingResponse();
 
-  // Get the stats from admin-messages.js
-  let stats = statistics;
-  // Find a random category
-  let randCat = stats[Math.floor(Math.random() * stats.length)];
-  console.log(`Found a random category:\n>>> ${randCat.category}`)
-  // Pull a random entry from a category
-  let data = randCat.data;
-  let randStat = data[Math.floor(Math.random() * data.length)]
-  // And get the stat itself and the source
-  let stat = randStat.text
-  let source = randStat.source;
-  console.log(`Found a random stat:\n>>> ${stat}`)
-  console.log(`with source\n>>> ${source}`)
+  let stat;
+  let source;
 
-  // Make a new message out of them and append it to our response.
+  if (firstWord == 'random') {
+    let rand = getRandomStat();
+    stat = rand[0];
+    source = rand[1];
+  } else {
+    // Get the keywords from the text
+    let keywords = getKeywords(msg);
+    // Find the matches and return a random one
+    let match = random(matchSearch(keywords));
+    // Send the first matched text with source
+    if (match) {
+      stat = match.text;
+      source = ` // source: ${match.source}`;
+    } else {
+      stat = "Thanks for texting Conversation Starter.  It appears we don't have information on that issue at this time, but you can slip us your own facts via DM: https://twitter.com/conv_starter";
+      source = "";
+    }
+  }
+
+  // Make a new message and append it to our response.
   let response = `${stat}
   ${source}`
   resp.message(response);
@@ -90,8 +122,8 @@ app.post('/sms', function (req, res) {
   res.end(resp.toString());
 
 
-  textAdmin(`${fromNum} : ${response}`);
-  sendMsg(fromNum, response);
+  // textAdmin(`${fromNum} : ${response}`);
+  // sendMsg(fromNum, response);
 
 });
 
@@ -134,4 +166,100 @@ function textAdmin(msg) {
     console.log(`SMS sent to Admin ${adminNum}\n>>> ${msg}`);
 
   });
+}
+
+function getKeywords(str){
+
+  let keywords = [];
+
+  var params = {
+    ignoreStopWords: true,
+    ignoreCase: true,
+    ignorePunctuation: true
+  };
+
+  let concord = RiTa.concordance(str, params)
+
+  for (var word in concord) {
+    if (concord.hasOwnProperty(word)) {
+      var tags = RiTa.getPosTags(word);
+      if ( tags[0] == 'nn' ||
+           tags[0] == 'nns' ||
+           tags[0] == 'jj' ) {
+        keywords.push(word);
+      }
+    }
+  }
+
+  console.log(`Keywords: ${keywords}`)
+  return keywords
+
+}
+
+function matchSearch(words) {
+  // let words = w.split(",")
+  // console.log(words)
+  var matches = []
+
+  // For each of the words in the msg
+  for (word of words) {
+    // console.log(word);
+
+    // Check the stats for tags
+    for (var k of statistics.data) {
+      let tags = k.tags;
+      // console.log(word, tags);
+
+      // Check each tag to see if it's a match with our word
+      for (tag of tags) {
+
+        // if it is a match, store the matched object in an array
+        if (word == tag) {
+        // console.log(k.text)
+
+          // Return the text from that matched object
+          if (!matches.find(e => e == k.text)) {
+              // console.log("new item found")
+              matches.push({
+                text: k.text,
+                source: k.source,
+              })
+          }
+        }
+      }
+    }
+  }
+
+  return matches
+
+}
+
+function getRandom() {
+
+  console.log(`Getting random stat and source...`)
+
+  // Get the stats from admin-messages.js
+  let stats = statistics;
+
+  // Find a random category
+  let randCat = stats[Math.floor(Math.random() * stats.length)];
+  // console.log(`Found a random category:\n>>> ${randCat.category}`)
+
+  // Pull a random entry from a category
+  let data = randCat.data;
+  let randStat = data[Math.floor(Math.random() * data.length)]
+
+  // And get the stat itself and the source
+  let stat = randStat.text
+  let source = randStat.source;
+
+  console.log(`Found a random stat:\n>>> ${stat}`)
+  console.log(`with source\n>>> ${source}`)
+
+  return [stat, source]
+}
+
+function random(array) {
+  rand = array[Math.floor(Math.random() * array.length)]
+  return rand
 }
