0b5033e10beda08bed139c63e0ae9849e61a255f
billythemusical <bennett.billy@gmail.com>
2020-06-18 21:35:26 -0400

    Updating statistics for one word searches.  How to do phrases?

--- statistics.js
+++ statistics.js
@@ -5,25 +5,25 @@ const statistics = {
       category: "incarceration",
       text:"In 2014, African Americans constituted 2.3 million, or 34%, of the total 6.8 million correctional population.",
       source:"https://www.naacp.org/criminal-justice-fact-sheet/",
-      tags: ["incarceration", "prison", "industrial complex", "jail"]
+      tags: ["incarceration", "prison", "industrial", "complex", "jail"]
     },
     {
       category: "poverty",
       text:"The median household income among black Americans is still dramatically lower than among white Americans ($40,065, as compared to $65,041).",
       source:"https://www.epi.org/publication/50-years-after-the-kerner-commission/",
-      tags: [ "poverty", "inequality" ]
+      tags: [ "poverty", "inequality", "inequity" ]
     },
     {
       category: "health",
       text:"The infant mortality rate in America for per 1000 births is more than twice as high for black infants (11.4) as compared to white infants (4.9).",
       source:"https://www.epi.org/publication/50-years-after-the-kerner-commission/",
-      tags: [ "infant mortality", "health" ]
+      tags: [ "infant", "mortality", "health" ]
     },
     {
       category: "health",
       text:"An African American born today can, on average, expect to live about 3.5 fewer years than a white person born on the same day.",
       source:"https://www.epi.org/publication/50-years-after-the-kerner-commission/#_note11",
-      tags: [ "life expectancy", "health" ]
+      tags: [ "life", "expectancy", "health" ]
     }
   ]
 }
